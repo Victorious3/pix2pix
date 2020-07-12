9eed66377e4351f91ae5449f3076475ccf0a15d0
Victorious3 <vic@nightfall.moe>
2020-07-12 15:21:47 +0200

    Add discriminator

--- models.py
+++ models.py
@@ -1,2 +1,45 @@
 import torch.nn as nn
 import torch
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels * 2, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.out = nn.Sequential(
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img_a, img_b):
+        input_img = torch.cat((img_a, img_b), 1)
+        x = self.layer1(input_img)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.out(x)
+        return x
